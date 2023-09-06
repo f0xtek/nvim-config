0967e2cf76e62e08dd3ed0ea478fe99e9e3a83c5
Luke Anderson <landerson61@gmail.com>
2023-09-06 13:02:41 +0100

    Fix plugin sync command

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 # Install packer plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-nvim +PackerSync +qa
+nvim +PackerSync
